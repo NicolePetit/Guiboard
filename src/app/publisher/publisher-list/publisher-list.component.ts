b0ab14b94dee7b0451b217a37dc3d3cd4ca6ca1d
Nicole Petit <nicole.petit87@gmail.com>
2022-05-10 17:21:46 +0800

    add filter and sticky header

--- src/app/publisher/publisher-list/publisher-list.component.ts
+++ src/app/publisher/publisher-list/publisher-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 import { Publisher } from '../publisher.model';
 import { PublisherService } from '../publisher.service';
@@ -12,7 +13,7 @@ import { PublisherService } from '../publisher.service';
 export class PublisherListComponent implements OnInit {
   publishers :Publisher[]=this.publservice.publishers;
   displayedColumns: string[] = ['name', 'surname', 'gender'];
-  dataSource = this.publishers;
+  dataSource = new MatTableDataSource(this.publishers);
   sub: Subscription;
 
   constructor( public publservice : PublisherService) { }
@@ -21,8 +22,16 @@ export class PublisherListComponent implements OnInit {
     this.sub = this.publservice.publishersChanged.subscribe(
       (publishers: Publisher[]) => {
         this.publishers = publishers;
+        this.dataSource = new MatTableDataSource(this.publishers);
+        this.dataSource.filterPredicate = (data: Publisher, filter: string) => {
+          return data.lastName.toLowerCase().includes(filter) || data.firstName.toLowerCase().includes(filter) || data.gender.toLowerCase().includes(filter)
+        };
       }
     );
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
